5e0727865f55fb97403d6d8995861e95781ba7c4
WillRMorris <153463826+WillRMorris@users.noreply.github.com>
2024-01-08 20:02:55 -0700

    reorganized for effciency

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,60 +1,63 @@
 // Assignments
-var upLetters = ["A","B","C","D","E","F","G","H","I","J","K",
-"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
-"k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specials = [
-  " ", "!", `"`, "#", "$", "%", "&", "'", 
-  "(", ")", "*", "+", ",", "-", ".", "/", 
-  ":", ";", "<", "=", ">", "?", "@", "[", 
-  "/", "]", "^","_", "`", "{", "|", "}", "~"
-];
-var random= "";
-var password = "";
-var passwordLength= "50";
+const questionPop = document.querySelector("#questions-pop");
+const questionClose = document.querySelector("#q-close");
+const numCheck = document.getElementById("num-check");
+const upLetterCheck = document.getElementById("up-letter-check");
+const lowLetterCheck = document.getElementById("low-letter-check");
+const specCheck = document.getElementById("spec-check");
+const stringLength = document.getElementById("string-length");
+const generateBtn = document.querySelector("#generate");
+
 var hasNumbers = false;
 var hasSpecials = false;
 var hasUpLetters = false;
 var hasLowLetters = false;
 
-
-// references
-var questionPop = document.querySelector("#questions-pop");
-var questionClose = document.querySelector("#q-close");
-var numCheck = document.getElementById("num-check");
-var upLetterCheck = document.getElementById("up-letter-check");
-var lowLetterCheck = document.getElementById("low-letter-check");
-var specCheck = document.getElementById("spec-check");
-var stringLength = document.getElementById("string-length");
-var generateBtn = document.querySelector("#generate");
-
 //function to grab random value from each array
 function getUpLetter() {
+  var upLetters = ["A","B","C","D","E","F","G","H","I","J","K",
+  "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var random =  "";
   random = upLetters[Math.floor(Math.random() * upLetters.length)];
-  return;
+  return random;
 }
 function getLowLetter() {
+  var lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 
+  "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var random =  "";
   random = lowLetters[Math.floor(Math.random() * lowLetters.length)];
-  return;
+  return random;
 }
 function getNumber() {
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var random =  "";
   random = numbers[Math.floor(Math.random() * numbers.length)];
-  return;
+  return random;
 }
 function getSpecial() {
+  var specials = [
+    " ", "!", `"`, "#", "$", "%", "&", "'", 
+    "(", ")", "*", "+", ",", "-", ".", "/", 
+    ":", ";", "<", "=", ">", "?", "@", "[", 
+    "/", "]", "^","_", "`", "{", "|", "}", "~"
+  ];
+  var random =  "";
   random = specials[Math.floor(Math.random() * specials.length)];
-  return;
+  return random;
 }
 // primary function--generates password
 function generatePassword() {
+  var password = ""
+  var random = ""
+  var passwordLength = "";
   // checks for invalid input and averts it
   passwordLength = Number(stringLength.value);
   if (!hasUpLetters && !hasLowLetters && !hasLowLetters && !hasSpecials){
-    hasLowLetters=true;
+    alert ( "please choose at least one option");
   }
   if (typeof passwordLength != typeof 8 || passwordLength < 1) {
-    passwordLength = 8;
+    alert ( "please enter valid password length");
+    
   }
   else if (passwordLength > 128) {
     passwordLength = 128;
@@ -62,10 +65,10 @@ function generatePassword() {
   // creates temporary cycle for randomization
   var ranCycle = [];
   var ranHolder;
-
+  
   // trouble shooting for password length
   console.log( "password length: " + passwordLength);
-
+  
   //creates a placeholder for each array
   // that the user indicates they want in their password
   if (hasUpLetters){
@@ -86,16 +89,16 @@ function generatePassword() {
   for (i = 0; i< passwordLength; i++ ) {
     ranHolder = ranCycle [Math.floor(Math.random() * ranCycle.length)];
     if (ranHolder == 0) {
-      getUpLetter();
+      random = getUpLetter();
     }
     else if (ranHolder == 1) {
-      getLowLetter();
+      random = getLowLetter();
     }
     else if (ranHolder == 2) {
-      getNumber();
+      random = getNumber();
     }
     else if (ranHolder == 3) {
-      getSpecial();
+      random = getSpecial();
     }
     else {
       // for kicks and giggles if someone manages to brear this
@@ -108,19 +111,16 @@ function generatePassword() {
     password += random;
     console.log ( "digit: " + random);
   }
-  return;
+  return password;
 }
 // Write password to the #password input
 function writePassword() {
-  generatePassword();
+  var password = generatePassword();
 
   // trouble shooting for password generation 
   console.log("password: " + password);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
-  //resets password
-  password = "";
   return;
 };
 
